a06aa8f406a4c2b07c5379914f6651727fbd7260
brian ally <brian@zijn-digital.com>
2017-06-20 13:16:55 -0400

    addedauth to controllers

--- app/Http/Controllers/SurgeonController.php
+++ app/Http/Controllers/SurgeonController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class SurgeonController extends Controller
 {
     /**
+     * constructor
+     * 
+     * @return  void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
