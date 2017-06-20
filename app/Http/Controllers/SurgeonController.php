9bcb0fdc416c99892e5aa81610c84339a5183c12
brian ally <brian@zijn-digital.com>
2017-06-20 12:44:28 -0400

    added controllers

---
+++ app/Http/Controllers/SurgeonController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Surgeon;
+use Illuminate\Http\Request;
+
+class SurgeonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Surgeon  $surgeon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Surgeon $surgeon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Surgeon  $surgeon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Surgeon $surgeon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Surgeon  $surgeon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Surgeon $surgeon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Surgeon  $surgeon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Surgeon $surgeon)
+    {
+        //
+    }
+}
