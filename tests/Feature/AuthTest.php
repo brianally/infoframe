f0725213b9a69fe81ae9437e3d15ec1d860429fc
brian ally <brian@zijn-digital.com>
2017-06-22 17:14:52 -0400

    login/logout tests

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+use Tests\DatabaseSeed;
+use Tests\DefaultUser;
+
+class AuthTest extends TestCase
+{
+    use DatabaseSeed;
+    use DefaultUser;
+
+    /**
+     * redirects to login if not authorised.
+     *
+     * @return void
+     * @test
+     */
+    public function test_it_redirects_to_login()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(302);
+
+        $response->assertRedirect('/login');
+    }
+
+
+    /**
+     * default user is authorised.
+     *
+     * @return void
+     * @test
+     */
+    public function test_default_login()
+    {
+        $data = ['email' => 'default@test.net', 'password' => 'default'];
+
+        $response = $this->post('/login', $data);
+
+        $response->assertStatus(302);
+
+        $response->assertRedirect('/home');
+    }
+
+
+    /**
+     * logout redirects and removes authorisation
+     *
+     * @return void
+     * @test
+     */
+    public function test_logout()
+    {
+        $user = $this->getDefaultUser();
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertStatus(302);
+
+        $response->assertRedirect('/');
+    }
+}
